fd5955032fcca3c2cac52fec06a1eaba9bde30b9
akamensi <akez.muhammed@gmail.com>
2024-08-02 19:54:31 +0200

    Setting Up The Project and Add Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Heading from "./ui/Heading";
+import Input from "./ui/Input";
+import Row from "./ui/Row";
+
+const StyledApp = styled.main`
+  /* background-color: orangered; */
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello aka</div>;
+  return (
+    <>
+      <GlobalStyles />
+      {/* <GlobalStyles /> This component doesn't accept any children, it's a
+      selfClosin Component, it's need to be a sibling of all others components*/}
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <div>
+              <Heading as="h1">The wooden Hotel</Heading>
+              <Heading as="h2">wooden</Heading>
+              <Button>check in</Button>
+              <Button variation="secondary" size="small">
+                check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Hotel</Heading>
+            <div>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </div>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
