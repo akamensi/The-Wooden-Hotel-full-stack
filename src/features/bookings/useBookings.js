e485344ebb6237e90682d334071703c403d73cb7
akamensi <akez.muhammed@gmail.com>
2024-08-18 17:02:20 +0200

    API-Side Sorting Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -13,13 +13,18 @@ function useBookings() {
       : { field: "status", value: filterValue };
   //:{field: "status", value: 5000, method:"gte"}
 
+  //Sort
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { isLoading, error, bookings };
 }
