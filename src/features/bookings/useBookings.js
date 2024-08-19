e5705b3ebf193c7161540d85fd34d2f212d0f8dc
akamensi <akez.muhammed@gmail.com>
2024-08-19 16:29:09 +0200

    API-Side Pagination Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -18,15 +18,18 @@ function useBookings() {
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
 
+  //Pagination
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
   const {
     isLoading,
-    data: bookings,
+    data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
-  return { isLoading, error, bookings };
+  return { isLoading, error, bookings, count };
 }
 
 export default useBookings;
