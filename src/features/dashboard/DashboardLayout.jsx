703f22a7fbc0120fa4ec9141014dcdb30853d05f
akamensi <akez.muhammed@gmail.com>
2024-08-24 18:23:19 +0200

    Computing Recent Bookings and Stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Spinner from "../../ui/Spinner";
+import { useRecentBookigs } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,6 +11,11 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { isLoading: isLoading1, bookings } = useRecentBookigs();
+  const { isLoading: isLoading2, stays, confirmedStays } = useRecentStays();
+  if (isLoading1 || isLoading2) return <Spinner />;
+
+  console.log(bookings);
   return (
     <StyledDashboardLayout>
       <div>Staistics</div>
